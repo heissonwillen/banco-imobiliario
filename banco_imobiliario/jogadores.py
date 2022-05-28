05c4746192adecf0c4680c4ef41283148cb80a59
Heisson Willen <heisson70@gmail.com>
2022-05-28 12:08:28 -0300

    feat: interacao de jogador com propriedade

--- banco_imobiliario/jogadores.py
+++ banco_imobiliario/jogadores.py
@@ -1,4 +1,15 @@
+from banco_imobiliario.propriedade import Propriedade
+
+
 class JogadorBase():
     def __init__(self, saldo: int = 300) -> None:
         self.saldo = saldo
         self.posicao = 0
+        
+    def interage_com_propriedade(self, prop: Propriedade) -> None:
+        if prop.dono and prop.dono != self:
+            self.paga(prop.valor_aluguel, prop.dono)
+        else:
+            if self.decide_compra(prop):
+                self.paga(prop.custo_venda)
+                prop.dono = self
